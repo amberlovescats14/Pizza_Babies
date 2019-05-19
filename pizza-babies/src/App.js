e9af8012c250fcafa8bd5948f0e98e1732580630
Amber Jones <amberlovescats14@gmail.com>
2019-05-19 16:02:19 -0500

    times conversion

--- pizza-babies/src/App.js
+++ pizza-babies/src/App.js
@@ -1,23 +1,5 @@
 import React , { Component } from 'react';
 import './App.css';
-<<<<<<< HEAD
-import Router from '../src/components/Router';
-import Header from './components/Header';
-import {BrowserRouter, Route} from 'react-router-dom';
-
-
-export default class App extends Component {
-  render(){
-  return (
-
-    <BrowserRouter>
-    <h1>hello</h1>
-    <Header/>
-    <Router/>
-    
-    </BrowserRouter>
-  );
-=======
 import Header from './components/Header'
 import Router from './components/Router'
 import {BrowserRouter, Route} from 'react-router-dom'
@@ -36,7 +18,6 @@ export default class App extends Component {
       </div>
     );
     }
->>>>>>> 1dfada98da2d540317b560d7be3e312c6586f5d4
   }
 
 
--- pizza-babies/src/components/Time.js
+++ pizza-babies/src/components/Time.js
@@ -1,22 +1,44 @@
 import React, { Component } from 'react'
 const containerStyle = {
-  color: 'white',
-  background: 'red',
+  boxShadow: '0 0 4px 1px rgba(0, 0, 0, 0.3)',
   width: '50%',
   margin: 'auto',
-  fontSize: '5vh'
+  height: '100px'
+}
+const styleMinutes = {
+  borderBottom: '3px solid red',
+  height: '50%',
+  display: 'flex',
+  justifyContent: 'center',
+  fontSize: '4vh'
+}
+const styleHours = {
+  borderBottom: '3px solid red',
+  height: '50%',
+  display: 'flex',
+  justifyContent: 'center',
+  fontSize: '4vh'
 }
 
 
 export default class Time extends Component {
   state = {
     typingState: "",
-    resultState: ""
+    hourState: "",
+    minuteState: ""
   }
   handleInput = (e) => {
     let value = document.getElementById('minutes').value
     let equation = Number(value) / Number(60)
-    this.setState({ typingState: e.target.value , resultState:equation });
+    this.setState({ typingState: e.target.value , hourState:equation });
+  }
+  hoursToMinutes = (e) => {
+    let value = e.target.value;
+    let equation = Number(value) * 60
+    console.log(equation)
+    this.setState({ minuteState: e.target.value  , typingState: equation});
+    // let equation = Number(60) / Number(value);
+    // this.setState({ typingState: e.target.value, hourState: equation  });
   }
   
 
@@ -25,10 +47,14 @@ export default class Time extends Component {
     return (
       <div id="container" style={containerStyle}>
         <div className="calculator" >
+        <div className="minutes" style={styleMinutes}>
         <label for="minutes">Minutes</label>
         <input placeholder="minutes" id="minutes" onChange={this.handleInput} value={this.state.typingState}></input>
-        <p id="hours">Hours</p>
-        <input placeholder="Hours" id="hours" value={this.state.resultState} ></input>
+        </div>
+        <div className="hours" style={styleHours}>
+        <label for="Hours">Hours</label>
+        <input placeholder="Hours" id="hours" value={this.state.hourState} onChange={this.hoursToMinutes}></input>
+        </div>
         </div>
       </div>
     )
