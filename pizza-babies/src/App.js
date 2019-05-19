23560e780c034af905978ce5753ecc16cd51dc7f
Michael Snowden <mtsnowden@gmail.com>
2019-05-19 13:41:38 -0500

    built header layout

--- pizza-babies/src/App.js
+++ pizza-babies/src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Header from './components/Header';
 import './App.css';
+import {BrowserRouter} from 'react-router-dom';
+import Router from './Router';
 
-function App() {
-  return (
-    <div className="App">
-    <h1>Pizza Babies
-    </h1>
-    </div>
-  );
+class App extends Component  {
+  render() {
+   return (
+      <div className="App">
+      <h1>Pizza Babies
+      </h1>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+    
+
+
+
---
+++ pizza-babies/src/components/Header..js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./header.css";
+import { Link } from 'react-router-dom';
+
+const Header = () => (
+  <div className="header">
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/length">Length</Link></li>
+      <li><Link to="/Time">Time</Link></li>
+      <li><Link to="/Volume">Volume</Link></li>
+    </ul>
+  </div>
+)
+
+export default Header;
