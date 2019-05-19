1dfada98da2d540317b560d7be3e312c6586f5d4
Amber Jones <amberlovescats14@gmail.com>
2019-05-19 14:25:47 -0500

    fixed it

--- pizza-babies/src/App.js
+++ pizza-babies/src/App.js
@@ -1,24 +1,47 @@
 import React , { Component } from 'react';
 import './App.css';
-import Router from '../src/components/Router'
 import Header from './components/Header'
-import {BrowserRouter, Route} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 
 
 export default class App extends Component {
-  render(){
-  return (
-    <BrowserRouter>
-    <div>
-    <h1>hello</h1>
-    <Header/>
-    <Router/>
-    </div>
-    
-  </BrowserRouter>
-  );
+    render(){
+    return (
+      <div>
+        <BrowserRouter>
+         <Header/>
+
+        </BrowserRouter>
+
+
+      </div>
+    );
+    }
   }
-}
+
+
+
+
+// import './App.css';
+// import Router from '../src/components/Router'
+// import Header from './components/Header'
+// import {BrowserRouter} from 'react-router-dom'
+
+
+// export default class App extends Component {
+//   render(){
+//   return (
+//     <BrowserRouter>
+//     <div>
+//     <h1>hello</h1>
+//     <Header/>
+//     <Router/>
+//     </div>
+    
+//   </BrowserRouter>
+//   );
+//   }
+// }
 
 
 
--- pizza-babies/src/components/Header.js
+++ pizza-babies/src/components/Header.js
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom';
 
 const Header = () => (
   <div className="header">
+  <h1>header</h1>
     <ul>
       <li><Link to="/">Home</Link></li>
       <li><Link to="/length">Length</Link></li>
