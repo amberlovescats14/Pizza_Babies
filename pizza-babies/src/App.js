ab8f07ce72a8532698e4c382c8e35a79648478da
Michael Snowden <mtsnowden@gmail.com>
2019-05-19 14:05:05 -0500

    ;lkj

--- pizza-babies/src/App.js
+++ pizza-babies/src/App.js
@@ -1,22 +1,17 @@
 import React , { Component } from 'react';
 import './App.css';
-import Router from '../src/components/Router'
-import Header from './components/Header'
-import {BrowserRouter, Route} from 'react-router-dom'
+import Router from '../src/components/Router';
+import Header from './components/Header.';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 
-class App extends Component {
+export default class App extends Component {
   render(){
   return (
     <BrowserRouter>
     <Header/>
     <Router/>
     
-  </BrowserRouter>
+    </BrowserRouter>
   );
-
-
-export default App;
-
-    
-
+  }}
